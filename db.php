45fc5f4f59e9867a666de76fab88da5ff4fae8d7
LT-Tommo <loritotommaso@gmail.com>
2025-04-04 08:49:32 +0200

    work in progress

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+
+$dbHost = "127.0.0.1";  # Indirizzo "localhost"
+$dbUser = "root";       # L'utente del DBMS
+$dbPass = "";           # La password dell'utente del DBMS
+$dbName = "test";       # Il nome del database da selezionare
--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+    include_once "db.php";
+    
+    if ($_GET["err"] == 1) {
+        echo "Username o Password non validi!";
+    }
+
+?>
+<head>
+<link rel="stylesheet" href="login.php"> 
+</head>
+<form method="POST" action="login-action.php">
+    <label for="Username">Username</label> <br>
+    <input name="Username" type="text"> <br><br>
+
+    <label for="Password">Password</label> <br>
+    <input name="Password" type="password"> <br><br>
+
+    <input type="submit" value="Accedi">
+</form>
