d098f606a162680fc2fdb20f70ff78a63a7c31d3
LT-Tommo <loritotommaso@gmail.com>
2025-04-04 10:02:41 +0200

    login v1

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
     include_once "db.php";
-    
-    if ($_GET["err"] == 1) {
-        echo "Username o Password non validi!";
-    }
-
 ?>
+
 <head>
-<link rel="stylesheet" href="login.php"> 
+<link rel="stylesheet" href="/ADP-VLC/style/login.css"> 
 </head>
-<form method="POST" action="login-action.php">
-    <label for="Username">Username</label> <br>
-    <input name="Username" type="text"> <br><br>
+<body>
+    <div id="containerLogin">
+        <form method="POST" action="login-action.php">
+            <label for="Username">Username</label> <br>
+            <input name="Username" type="text"> <br><br>
 
-    <label for="Password">Password</label> <br>
-    <input name="Password" type="password"> <br><br>
+            <label for="Password">Password</label> <br>
+            <input name="Password" type="password"> <br><br>
 
-    <input type="submit" value="Accedi">
-</form>
+            <input type="submit" value="Accedi">
+        </form>
+    </div>
+</body>
+    
+
