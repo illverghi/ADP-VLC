461c0ccc6b21e8862b5c9658c0257499bba37b2b
illverghi <alessandrovergati22@gmail.com>
2025-05-07 19:18:47 +0200

    employee modify

--- employee.php
+++ employee.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <?php
-            session_start();
-
-            /*
-            if (isset($_POST['codFiscale'])) {
-                $_SESSION['codFiscale'] = $_POST['codFiscale'];
-            }
-
-            if (isset($_SESSION['codFiscale'])) {
-                echo "<h1>" . $_SESSION['codFiscale']."</h1>";
-            } else {
-                echo "Nessun dipendente trovato nella sessione.";
-            }*/
-        ?>
+    include_once "db.php";
+    session_start();
+    if (isset($_POST['codFiscale'])) {
+        $_SESSION['codFiscale'] = $_POST['codFiscale'];
+        $codFiscale = $_SESSION['codFiscale'];
+    }
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -32,7 +26,46 @@
                         Chi Siamo?
                     </div>
                 </a>
+                |
+                <a href="company-private.php">
+                    <div class="toolbar-arrow">
+                        <img src="/ADP-VLC/style/drawable/bee-backArrow.png" alt="Arrow" width="40" height="40">
+                    </div>
+                </a>
             </div>
         </header>
+        <?php
+            $dbConn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+            if ($dbConn) {
+            
+                // La query da eseguire
+                $querySql = "SELECT nome, cognome FROM dipendenti WHERE codFiscale = '" . $codFiscale . "'";
+            
+                // Eseguo la query
+                $queryRes = mysqli_query($dbConn, $querySql);
+            
+                // Se NON ci sono errori
+                if ($queryRes) {
+                    if (mysqli_num_rows($queryRes) === 1) {
+                        // Prendo il recordo dal DB
+                        $row = mysqli_fetch_assoc($queryRes);
+            
+                        // Memorizzo i dati nelle variabili di sessione.
+                        $_SESSION["nome"] = $row["nome"];
+                        $_SESSION["cognome"] = $row["cognome"];
+                        echo "<div id='employee-name'>{$_SESSION['nome']} {$_SESSION['cognome']}</div>";
+                    }
+                }
+                else {
+                    echo "Errore nella query: " . mysqli_error($dbConn);
+                }
+            
+                // Chiudo la connessione al DB
+                mysqli_close($dbConn);
+            }
+            else {
+                echo "Errore nella connessione al DB.";
+            }
+        ?>
     </body>
 </html>
