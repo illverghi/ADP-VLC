70096f24e3f107f95c946523264fd0558067dbf6
illverghi <alessandrovergati22@gmail.com>
2025-05-07 18:19:38 +0200

    employee start

--- employee.php
+++ employee.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<?php
+            session_start();
+
+            /*
+            if (isset($_POST['codFiscale'])) {
+                $_SESSION['codFiscale'] = $_POST['codFiscale'];
+            }
+
+            if (isset($_SESSION['codFiscale'])) {
+                echo "<h1>" . $_SESSION['codFiscale']."</h1>";
+            } else {
+                echo "Nessun dipendente trovato nella sessione.";
+            }*/
+        ?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -6,18 +21,18 @@
         <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
     </head>
     <body>
-        <?php
-            session_start();
-            /*
-            if (isset($_POST['codFiscale'])) {
-                $_SESSION['codFiscale'] = $_POST['codFiscale'];
-            }
-
-            if (isset($_SESSION['codFiscale'])) {
-                echo "<h1>" . $_SESSION['codFiscale']."<h1";
-            } else {
-                echo "Nessun dipendente trovato nella sessione.";
-            }*/
-        ?>
+        <header>
+            <div id="toolbar">
+                <div id="toolbar-logo">
+                    <img src="/ADP-VLC/style/drawable/logoBeeEfficient(2).png" alt="Logo" width="80" height="30">
+                </div>
+                |
+                <a href="">
+                    <div class="toolbar-item">
+                        Chi Siamo?
+                    </div>
+                </a>
+            </div>
+        </header>
     </body>
 </html>
